7d24daceddcd85b9134cd467e6a1129688a16d36
Rilwanu Idris <Rillygulma@gmail.com>
2024-12-23 22:28:48 +0100

    Update: [Fix icons error]

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -8,25 +8,54 @@ import {
   FaSync,
   FaPlus,
   FaSearch,
+  FaCode,
 } from "react-icons/fa";
 import { IoIosCall } from "react-icons/io";
 import { TiHomeOutline } from "react-icons/ti";
-import { FaCode } from "react-icons/fa6";
 
 import Logo from "../assets/CodeAnt.png";
 
-const Dashboard = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+interface Repo {
+  name: string;
+  type: string;
+  size: string;
+  lang: string;
+  updated: string;
+}
+
+interface NavigationItem {
+  icon: React.ComponentType;
+  label: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+  const repositories: Repo[] = [
+    { name: "design-system", type: "Public", size: "7320 KB", lang: "React", updated: "1 day ago" },
+    { name: "codeant-ci-app", type: "Private", size: "5871 KB", lang: "JavaScript", updated: "2 days ago" },
+    { name: "analytics-dashboard", type: "Private", size: "4521 KB", lang: "Python", updated: "5 days ago" },
+    { name: "mobile-app", type: "Public", size: "3096 KB", lang: "Swift", updated: "3 days ago" },
+    { name: "e-commerce-platform", type: "Private", size: "6210 KB", lang: "Java", updated: "6 days ago" },
+  ];
+
+  const navigationItems: NavigationItem[] = [
+    { icon: TiHomeOutline, label: "Repositories" },
+    { icon: FaCode, label: "AI Code Review" },
+    { icon: FaCloud, label: "Cloud Security" },
+    { icon: FaQuestionCircle, label: "How to Use" },
+    { icon: FaCog, label: "Settings" },
+  ];
+
   return (
     <div className="min-h-screen bg-gray-100">
       {/* Header for Small Devices */}
-      <div className="p-4 flex justify-between items-center lg:hidden bg-white border ">
-        {isSidebarOpen ? null : (
+      <div className="p-4 flex justify-between items-center lg:hidden bg-white border">
+        {!isSidebarOpen && (
           <div className="flex items-center">
             <img src={Logo} alt="Logo" className="w-10 h-10 object-contain" />
             <span className="text-lg font-bold ml-2 text-gray-700">CodeAnt AI</span>
@@ -50,13 +79,11 @@ const Dashboard = () => {
               : "hidden lg:block"
           } lg:col-span-1 bg-white text-black p-4 rounded-md border border-gray-200`}
         >
-          {/* Logo */}
           <div className="flex mb-4 lg:hidden">
             <img src={Logo} alt="Logo" className="w-12 h-12 object-contain" />
             <span className="text-xl font-bold mt-2 text-gray-700">CodeAnt AI</span>
           </div>
 
-          {/* User Selection Dropdown */}
           <select
             className="w-full p-2 text-lg text-gray-500 border border-gray-300 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-gray-500"
             defaultValue="UkTarshDhaiyaPanwar"
@@ -66,18 +93,11 @@ const Dashboard = () => {
             <option value="JaneSmith">Jane Smith</option>
           </select>
 
-          {/* Navigation */}
           <nav className="mt-6 space-y-4">
-            {[
-              { icon: TiHomeOutline, label: "Repositories" },
-              { icon: FaCode, label: "AI Code Review" },
-              { icon: FaCloud, label: "Cloud Security" },
-              { icon: FaQuestionCircle, label: "How to Use" },
-              { icon: FaCog, label: "Settings" },
-            ].map((item, index) => (
+            {navigationItems.map((item, index) => (
               <button
                 key={index}
-                className="flex items-center space-x-2 text-gray-500 text-xl hover:bg-blue-500 hover:text-white transition-colors p-2 rounded-md w-2/3"
+                className="flex items-center space-x-2 text-gray-700 text-xl hover:bg-blue-500 hover:text-white transition-colors p-2 rounded-md w-2/3"
               >
                 <item.icon />
                 <span>{item.label}</span>
@@ -85,28 +105,26 @@ const Dashboard = () => {
             ))}
           </nav>
 
-          {/* Support and Logout */}
           <div className="mt-20">
-            <button className="flex items-center space-x-2 text-xl text-gray-500 hover:bg-blue-500 hover:text-white transition-colors p-2 rounded-md w-2/3">
+            <button className="flex items-center space-x-2 text-xl text-gray-800 hover:bg-blue-500 hover:text-white transition-colors p-2 rounded-md w-2/3">
               <IoIosCall />
               <span>Support</span>
             </button>
-            <button className="flex items-center space-x-2 text-xl text-gray-500 mt-2 hover:bg-blue-500 hover:text-white transition-colors p-2 rounded-md w-2/3">
+            <button className="flex items-center space-x-2 text-xl text-gray-800 mt-2 hover:bg-blue-500 hover:text-white transition-colors p-2 rounded-md w-2/3">
               <FaSignOutAlt />
               <span>Logout</span>
             </button>
           </div>
         </aside>
 
-        {/* Main Section */}
         <main className="lg:col-span-3 bg-white p-6 rounded-md">
           <div className="flex justify-between items-center flex-wrap gap-4">
             <div>
               <h2 className="text-xl text-gray-700 font-bold">Repositories</h2>
               <p className="text-sm text-gray-500">33 total repositories</p>
-          </div>
+            </div>
             <div className="flex space-x-3">
-              <button className="flex items-center bg-gray-200 text-sm text-gray-700 px-4 py-2 rounded-md">
+              <button className="flex items-center bg-white text-sm border border-gray-300 text-gray-600 px-4 py-2 rounded-md">
                 <FaSync className="mr-2" />
                 Refresh All
               </button>
@@ -117,7 +135,6 @@ const Dashboard = () => {
             </div>
           </div>
 
-          {/* Search Input */}
           <div className="my-4 relative">
             <FaSearch className="absolute top-3 left-3 text-gray-400" />
             <input
@@ -127,15 +144,8 @@ const Dashboard = () => {
             />
           </div>
 
-          {/* Repository List */}
           <ul className="space-y-4">
-            {[
-              { name: "design-system", type: "Public", size: "7320 KB", lang: "React", updated: "1 day ago" },
-              { name: "codeant-ci-app", type: "Private", size: "5871 KB", lang: "JavaScript", updated: "2 days ago" },
-              { name: "analytics-dashboard", type: "Private", size: "4521 KB", lang: "Python", updated: "5 days ago" },
-              { name: "mobile-app", type: "Public", size: "3096 KB", lang: "Swift", updated: "3 days ago" },
-              { name: "e-commerce-platform", type: "Private", size: "6210 KB", lang: "Java", updated: "6 days ago" },
-            ].map((repo, index) => (
+            {repositories.map((repo, index) => (
               <li
                 key={index}
                 className="flex justify-between items-center bg-gray-50 p-4 border border-gray-0 rounded-md flex-wrap gap-4 hover:bg-gray-200 transition-colors"
@@ -147,13 +157,13 @@ const Dashboard = () => {
                       {repo.type}
                     </span>
                   </h3>
-                  <div className="text-xl text-gray-500 flex items-center space-x-4 flex-nowrap">
+                  <div className="text-xl text-gray-800 flex items-center space-x-4 flex-nowrap">
                     <span className="flex items-center space-x-2">
                       <span>{repo.lang}</span>
                       <span className="w-2 h-2 bg-blue-500 rounded-full inline-block"></span>
                     </span>
                     <span className="flex items-center space-x-2">
-                      <FaDatabase className="text-gray-500" />
+                      <FaDatabase className="text-gray-800" />
                       <span>{repo.size}</span>
                     </span>
                     <span className="flex items-center space-x-2">
--- src/components/SignInPage.tsx
+++ src/components/SignInPage.tsx
@@ -1,59 +1,59 @@
-import { FcPieChart } from "react-icons/fc";
-import { FcUp } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
-import { FaBitbucket } from "react-icons/fa";
+import React from "react";
+import { FcPieChart, FcUp } from "react-icons/fc";
+import { FaGithub, FaBitbucket } from "react-icons/fa";
 import { VscAzureDevops } from "react-icons/vsc";
 import { FaGitlab } from "react-icons/fa6";
-import Logo from "../assets/CodeAnt.png"
+import Logo from "../assets/CodeAnt.png";
+
+// Helper function for typing icons
+type IconProps = React.ComponentProps<"svg">;
+
+const PieChartIcon: React.FC<IconProps> = (props) => <FcPieChart {...props} />;
+const UpIcon: React.FC<IconProps> = (props) => <FcUp {...props} />;
+const GithubIcon: React.FC<IconProps> = (props) => <FaGithub {...props} />;
+const BitbucketIcon: React.FC<IconProps> = (props) => <FaBitbucket {...props} />;
+const AzureDevopsIcon: React.FC<IconProps> = (props) => <VscAzureDevops {...props} />;
+const GitlabIcon: React.FC<IconProps> = (props) => <FaGitlab {...props} />;
+
 const SignInPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="bg-white shadow-lg rounded-lg w-full max-w-md lg:max-w-4xl flex flex-col lg:flex-row relative overflow-hidden">
         {/* Left Content */}
-        <div className="hidden lg:flex flex-col justify-center p-8 w-1/2 bg-gray-50 relative">
-          {/* First Container */}
+        <div className="hidden lg:flex flex-col justify-center p-4 w-1/2 bg-gray-50 relative">
           <div className="flex flex-col space-y-4">
-            {/* Title and Badge */}
             <div className="relative flex flex-col bg-white shadow-lg rounded-lg p-6">
               <div className="flex items-center mb-2">
-                <img
-                  src={Logo} // Replace with the actual logo URL
-                  alt="CodeAnt AI Logo"
-                  className="h-12 mr-2"
-                />
-                <h2 className="text-xl font-bold">AI to Detect & Autofix Bad Code</h2>
+                <img src={Logo} alt="CodeAnt AI Logo" className="h-12 mr-2" />
+                <h2 className="text-sm text-gray-600 font-bold">
+                  AI to Detect & Autofix Bad Code
+                </h2>
               </div>
-              {/* Line Divider */}
               <div className="border-t border-gray-200 my-4"></div>
-
-              <ul className="flex text-gray-600 gap-3 space-x-3">
-                <li className="flex items-center">
-                  <span className="mr-2 text-green-500"></span>30+ 
-                  <br/>
-                  Language Support
+              <ul className="grid grid-cols-3 gap-4 text-gray-600 border border-gray-300 p-4 rounded-lg bg-gray-50 shadow-md">
+                <li className="flex flex-col items-center">
+                  <b className="text-lg text-gray-800">30+</b>
+                  <span className="text-xs">Language Support</span>
                 </li>
-                <li className="flex items-center">
-                  <span className="mr-2 text-green-500"></span>10K+ 
-                  <br/>
-                  Developers
+                <li className="flex flex-col items-center">
+                  <b className="text-lg text-gray-800">10K+</b>
+                  <span className="text-xs">Developers</span>
                 </li>
-                <li className="flex items-center">
-                  <span className="mr-2 text-green-500"></span>100K+ 
-                  <br/>
-                  Hours Saved
+                <li className="flex flex-col items-center">
+                  <b className="text-lg text-gray-800">100K+</b>
+                  <span className="text-xs">Hours Saved</span>
                 </li>
               </ul>
-              {/* Overlapping */}
-              <div className="relative mt-10">
+              <div className="relative bottom-4 left-40 mb-1" style={{ width: "220px" }}>
                 <div className="absolute top-0 left-0 w-full h-full bg-gray-200 rounded-lg"></div>
-                <div className="relative flex flex-col justify-between items-start bg-gray-50 shadow-md rounded-lg p-10 z-15">
-                  <FcPieChart />
+                <div className="relative flex flex-col justify-between items-start bg-gray-50 shadow-md rounded-lg p-6 z-15">
+                  <PieChartIcon className="text-3xl" />
                   <p className="text-sm text-gray-500">Issues Fixed</p>
                   <p className="text-2xl font-bold">500K+</p>
-                  <p className="text-sm flex items-end text-green-500 absolute right-0 top-10">
-                    <FcUp /> 14%
+                  <p className="text-sm flex font-bold items-end text-blue-700 absolute right-0 top-5">
+                    <UpIcon className="text-lg" /> 14%
                   </p>
-                  <span className="absolute right-0 top-16 text-sm text-gray-500">
+                  <span className="absolute right-0 top-10 text-sm text-gray-500">
                     This Week
                   </span>
                 </div>
@@ -61,50 +61,43 @@ const SignInPage: React.FC = () => {
             </div>
           </div>
         </div>
-
         {/* Right Content */}
         <div className="flex flex-col justify-center p-6 w-full lg:w-1/2">
-          <div className="text-center mb-8">
-            <img
-              src={Logo} // Replace with the actual logo URL
-              alt="CodeAnt AI Logo"
-              className="h-12 mx-auto"
-            />
-            <h1 className="text-2xl font-bold mt-4">Welcome to CodeAnt AI</h1>
+          <div className="flex items-center justify-center mb-8 space-x-1">
+            <img src={Logo} alt="CodeAnt AI Logo" className="h-12" />
+            <h1 className="text-sm text-gray-500 font-bold">CodeAnt AI</h1>
           </div>
-
-          {/* Switcher Buttons */}
-          <div className="flex space-x-2 mb-4 justify-center">
-            <button className="px-4 py-2 bg-blue-600 text-white font-bold rounded-lg hover:bg-blue-700">
+          <h1 className="text-xl text-center font-semibold mt-2">
+            Welcome to CodeAnt AI
+          </h1>
+          <div className="flex space-x-2 mb-4 justify-center mt-6">
+            <button className="px-4 py-2 bg-blue-600 w-1/2 text-gray-200 font-bold rounded-lg hover:bg-blue-700">
               SAAS
             </button>
-            <button className="px-4 py-2 bg-gray-200 text-gray-700 font-bold rounded-lg hover:bg-gray-300">
+            <button className="px-4 py-2 bg-gray-200 w-1/2 text-gray-500 font-bold rounded-lg hover:bg-gray-300">
               Self Hosted
             </button>
           </div>
-
-          {/* Login Buttons */}
           <div className="space-y-4">
             <button className="w-full py-2 bg-gray-100 border border-gray-300 rounded-lg flex items-center justify-center hover:bg-gray-200">
-            <FaGithub className="h-5 mr-2" />
+              <GithubIcon className="h-5 mr-2" />
               Sign in with GitHub
             </button>
             <button className="w-full py-2 bg-gray-100 border border-gray-300 rounded-lg flex items-center justify-center hover:bg-gray-200">
-            <FaBitbucket className="h-5 mr-2" />
+              <BitbucketIcon className="h-5 mr-2" />
               Sign in with Bitbucket
             </button>
             <button className="w-full py-2 bg-gray-100 border border-gray-300 rounded-lg flex items-center justify-center hover:bg-gray-200">
-            <VscAzureDevops className="h-5 mr-2" />
+              <AzureDevopsIcon className="h-5 mr-2" />
               Sign in with Azure DevOps
             </button>
             <button className="w-full py-2 bg-gray-100 border border-gray-300 rounded-lg flex items-center justify-center hover:bg-gray-200">
-            <FaGitlab className="h-5 mr-2" />
+              <GitlabIcon className="h-5 mr-2" />
               Sign in with GitLab
             </button>
           </div>
-
-          <p className="text-xs text-gray-500 mt-4 text-center">
-            By signing up you agree to the <span className="underline">Privacy Policy</span>.
+          <p className="text-xs text-gray-600 mt-4 text-center">
+            By signing up you agree to the <span className="font-bold">Privacy Policy</span>.
           </p>
         </div>
       </div>
